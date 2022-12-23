a9aaa4ef59cad3a2924f4e3028db6189b7c2f2f6
Bhargav <bhargavbalu63@gmail.com>
2022-12-23 09:09:45 +0530

    URL CHNAGED

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 dotenv.config();
 //connect to DB
 
-mongoose.connect('mongodb://localhost/instaclone',{ useNewUrlParser: true, useUnifiedTopology: true },() => {
+mongoose.connect('mongodb+srv://instaclone:9848032919@cluster0.615adal.mongodb.net/?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true },() => {
     console.log('connected to DB')})                                                                         // here blog is the database that is created and conenceted to mongodb
 
 
